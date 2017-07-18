e54f9d2da3c271d60773b1adef04438aa361e227
Slobodan Djordjevic <slobodan.djordjevic@tamedia.ch>
2017-07-18 17:03:02 +0200

    Adding material ui

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { render } from 'react-dom';
 import { browserHistory } from 'react-router';
 import { AppContainer } from 'react-hot-loader';
 import Root from './components/Root';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import configureStore from './store/configureStore';
 require('./favicon.ico'); // Tell webpack to load favicon.ico
@@ -18,9 +19,11 @@ const history = syncHistoryWithStore(browserHistory, store);
 
 
 render(
-  <AppContainer>
-    <Root store={store} history={history} />
-  </AppContainer>,
+  <MuiThemeProvider>
+    <AppContainer>
+      <Root store={store} history={history} />
+    </AppContainer>
+  </MuiThemeProvider>,
   document.getElementById('app')
 );
 
@@ -29,8 +32,9 @@ if (module.hot) {
     const NewRoot = require('./components/Root').default;
     render(
       <AppContainer>
-        <NewRoot store={store} history={history} />
-      </AppContainer>,
+          <NewRoot store={store} history={history} />
+      </AppContainer>
+      ,
       document.getElementById('app')
     );
   });

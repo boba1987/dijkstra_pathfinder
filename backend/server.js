4b1edddbfbc728e41bf4585d76e831e07a9fca61
Slobodan Djordjevic <slobodan.djordjevic@tamedia.ch>
2017-07-18 15:53:15 +0200

    Home component base class

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,11 @@ app.listen(9000, function () {
   console.log('Proxy server started at port 9000');
 });
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 var response = fs.readFileSync('response.json', {encoding: 'utf8'});
 
 app.get('/api', function(req, res) {
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import 'whatwg-fetch';
 
-const Home = () => {
-  return (
-    <div>
-      <h2>Get Started</h2>
-    </div>
-  );
-};
+class Home extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:9000/api')
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Get Started</h2>
+      </div>
+    );
+  }
+}
 
 export default Home;

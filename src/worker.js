719437ff05abee8cbc51ab78164bcfcbc3e775fd
Slobodan Djordjevic <slobodan.djordjevic@tamedia.ch>
2017-07-19 14:32:22 +0200

    Make worker.js test friendly

--- src/worker.js
+++ src/worker.js
@@ -1,133 +1,135 @@
-self.addEventListener('message', function(e) {
-  let response = e.data.paths;
-  /**
-   * Basic priority queue implementation.
-   */
-  function PriorityQueue () {
-      this._nodes = [];
+/**
+ * Basic priority queue implementation.
+ */
+function PriorityQueue () {
+    this._nodes = [];
 
-      this.enqueue = function (priority, key) {
-          this._nodes.push({key: key, priority: priority });
-          this.sort();
-      };
-      this.dequeue = function () {
-          return this._nodes.shift().key;
-      };
-      this.sort = function () {
-          this._nodes.sort(function (a, b) {
-              return a.priority - b.priority;
-          });
-      };
-      this.isEmpty = function () {
-          return !this._nodes.length;
-      };
-  }
+    this.enqueue = function (priority, key) {
+        this._nodes.push({key: key, priority: priority });
+        this.sort();
+    };
+    this.dequeue = function () {
+        return this._nodes.shift().key;
+    };
+    this.sort = function () {
+        this._nodes.sort(function (a, b) {
+            return a.priority - b.priority;
+        });
+    };
+    this.isEmpty = function () {
+        return !this._nodes.length;
+    };
+}
 
-  /**
-   * Pathfinding starts here
-   */
-  function Graph(){
-      let INFINITY = 1/0;
-      this.vertices = {};
+/**
+ * Pathfinding starts here
+ */
+function Graph(){
+    let INFINITY = 1/0;
+    this.vertices = {};
 
-      this.addVertex = function(name, edges){
-          this.vertices[name] = edges;
-      };
+    this.addVertex = function(name, edges){
+        this.vertices[name] = edges;
+    };
 
-      this.shortestPath = function (start, finish) {
-          let nodes = new PriorityQueue(),
-              distances = {},
-              previous = {},
-              path = [],
-              smallest, vertex, neighbor, alt;
+    this.shortestPath = function (start, finish) {
+        let nodes = new PriorityQueue(),
+            distances = {},
+            previous = {},
+            path = [],
+            smallest, vertex, neighbor, alt;
 
-          for(vertex in this.vertices) {
-              if(vertex === start) {
-                  distances[vertex] = 0;
-                  nodes.enqueue(0, vertex);
-              }
-              else {
-                  distances[vertex] = INFINITY;
-                  nodes.enqueue(INFINITY, vertex);
-              }
-
-              previous[vertex] = null;
-          }
-
-          while(!nodes.isEmpty()) {
-              smallest = nodes.dequeue();
-
-              if(smallest === finish) {
-                  path = [];
-
-                  while(previous[smallest]) {
-                      path.push(smallest);
-                      smallest = previous[smallest];
-                  }
-
-                  break;
-              }
-
-              if(!smallest || distances[smallest] === INFINITY){
-                  continue;
-              }
-
-              for(neighbor in this.vertices[smallest]) {
-                  alt = distances[smallest] + this.vertices[smallest][neighbor];
-
-                  if(alt < distances[neighbor]) {
-                      distances[neighbor] = alt;
-                      previous[neighbor] = smallest;
-
-                      nodes.enqueue(alt, neighbor);
-                  }
-              }
-          }
-
-          return path;
-      };
-  }
-
-  function getVertexInfo(response) {
-      let info = {};
-
-      response.forEach(function (deal) {
-        deal.durationMinutes = (parseInt(deal.duration.h)*60) + parseInt(deal.duration.m);
-        deal.priceTotal = deal.cost - (deal.cost*(deal.discount/100));
-        if (info[deal.departure]) {
-            if (info[deal.departure][deal.arrival]) {
-                info[deal.departure][deal.arrival].push(deal); // deal.cost - deal.discount    // deal.miliseconds     // etc.
-            } else {
-                info[deal.departure][deal.arrival] = [deal];
+        for(vertex in this.vertices) {
+            if(vertex === start) {
+                distances[vertex] = 0;
+                nodes.enqueue(0, vertex);
             }
-        } else {
-            info[deal.departure] = {};
-            info[deal.departure][deal.arrival] = [deal];
-        }
-      });
+            else {
+                distances[vertex] = INFINITY;
+                nodes.enqueue(INFINITY, vertex);
+            }
 
-      for (let dep in info) {
+            previous[vertex] = null;
+        }
+
+        while(!nodes.isEmpty()) {
+            smallest = nodes.dequeue();
+
+            if(smallest === finish) {
+                path = [];
+
+                while(previous[smallest]) {
+                    path.push(smallest);
+                    smallest = previous[smallest];
+                }
+
+                break;
+            }
+
+            if(!smallest || distances[smallest] === INFINITY){
+                continue;
+            }
+
+            for(neighbor in this.vertices[smallest]) {
+                alt = distances[smallest] + this.vertices[smallest][neighbor];
+
+                if(alt < distances[neighbor]) {
+                    distances[neighbor] = alt;
+                    previous[neighbor] = smallest;
+
+                    nodes.enqueue(alt, neighbor);
+                }
+            }
+        }
+
+        return path;
+    };
+}
+
+function reduceVertexInfo(info, e) {
+    for (let dep in info) {
         for (let arr in info[dep]) {
-          info[dep][arr].sort(function (l, r) {
-              return l[e.data.criteria] - r[e.data.criteria];
-          });
+            info[dep][arr] = info[dep][arr][0][e.data.criteria];
         }
-      }
+    }
 
-      return info;
-  }
+    return info;
+}
 
-  function reduceVertexInfo(info) {
-      for (let dep in info) {
-          for (let arr in info[dep]) {
-              info[dep][arr] = info[dep][arr][0][e.data.criteria];
+function getVertexInfo(response, e) {
+    let info = {};
+
+    response.forEach(function (deal) {
+      deal.durationMinutes = (parseInt(deal.duration.h)*60) + parseInt(deal.duration.m);
+      deal.priceTotal = deal.cost - (deal.cost*(deal.discount/100));
+      if (info[deal.departure]) {
+          if (info[deal.departure][deal.arrival]) {
+              info[deal.departure][deal.arrival].push(deal); // deal.cost - deal.discount    // deal.miliseconds     // etc.
+          } else {
+              info[deal.departure][deal.arrival] = [deal];
           }
+      } else {
+          info[deal.departure] = {};
+          info[deal.departure][deal.arrival] = [deal];
       }
+    });
 
-      return info;
-  }
+    for (let dep in info) {
+      for (let arr in info[dep]) {
+        info[dep][arr].sort(function (l, r) {
+            return l[e.data.criteria] - r[e.data.criteria];
+        });
+      }
+    }
 
-  let reduced_info = reduceVertexInfo(getVertexInfo(response)); // to avoid mutation
+    return info;
+}
+
+self.addEventListener('message', function(e) {
+  const data = e.data;
+  const response = data.paths;
+
+  let reduced_info = reduceVertexInfo(getVertexInfo(response, e), e); // to avoid mutation
 
   let graph = new Graph();
 
@@ -137,7 +139,7 @@ self.addEventListener('message', function(e) {
 
   const shortestPath = graph.shortestPath(e.data.from, e.data.to).concat([e.data.from]).reverse();
 
-  let info = getVertexInfo(response);
+  let info = getVertexInfo(response, e);
 
   let toReturn = [];
 
